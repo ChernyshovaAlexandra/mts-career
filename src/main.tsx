6f6ae11cec2a60a32d321827d178e255be0d3fc2
BastianHolmes <sebastian.holmes2001@gmail.com>
2025-07-25 17:54:39 +0300

    config: new lint and lib for a11y

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
+
+// Убрать при деплое в prod
+import("react-axe").then(axe => {
+  import("react-dom").then(ReactDOM => {
+    axe.default(React, ReactDOM, 1000);
+  });
+});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
