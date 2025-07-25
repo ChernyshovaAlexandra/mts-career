a08becea156ce48fb7c81514707e4a4bb6584b1e
BastianHolmes <sebastian.holmes2001@gmail.com>
2025-07-26 02:33:55 +0300

    feat: mobile view

--- src/shared/ui/FieldValue/index.tsx
+++ src/shared/ui/FieldValue/index.tsx
@@ -4,9 +4,13 @@ import styled from "styled-components";
 /**
  * @typedef {Object} FieldValueProps
  * @property {string} [className] - Дополнительные CSS классы
+ * @property {string} [id] - Уникальный ID для value
+ * @property {string} [aria-labelledby] - ID элемента, который описывает это значение
  */
 interface FieldValueProps extends PropsWithChildren {
   className?: string;
+  id?: string;
+  "aria-labelledby"?: string;
 }
 
 const Value = styled.span`
@@ -25,12 +29,22 @@ const Value = styled.span`
  *
  * Используется для всех значений полей в карточках с единообразным стилем.
  * Соответствует дизайн-системе MTS с указанными параметрами шрифта MTS Text.
+ * Поддерживает связь с соответствующими label элементами для улучшения доступности.
  *
  * @param props - Свойства компонента
  * @returns JSX элемент значения поля
  */
-const FieldValue: FC<FieldValueProps> = ({ children, className }) => {
-  return <Value className={className}>{children}</Value>;
+const FieldValue: FC<FieldValueProps> = ({
+  children,
+  className,
+  id,
+  "aria-labelledby": ariaLabelledby,
+}) => {
+  return (
+    <Value className={className} id={id} aria-labelledby={ariaLabelledby}>
+      {children}
+    </Value>
+  );
 };
 
 export default FieldValue;
