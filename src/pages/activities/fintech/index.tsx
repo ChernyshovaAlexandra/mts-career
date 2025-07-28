13ee5b0a29723784d45c6eba78acff4021dcbaeb
Alexandra <alexandrachrystal@gmail.com>
2025-07-28 20:49:58 +0700

    Add Quiz container and refactor QuizQuestion

--- src/pages/activities/fintech/index.tsx
+++ src/pages/activities/fintech/index.tsx
@@ -1,4 +1,4 @@
-import { QuizQuestion } from "../../../components";
+import { Quiz } from "../../../components";
 import { ActivityLayout } from "../../../layouts";
 import { applyNbsp } from "../../../utils";
 
@@ -9,30 +9,7 @@ const FintechPage = () => (
       "Знаешь, что чаевые в кафе можно оставить прямо с баланса телефона? А банковскую карту — оформить в пару кликов в Telegram? Изучи, как МТС делает финансы проще и удобнее и пройди короткий тест с выбором ответов. За каждый правильный — получай баллы!"
     )}
   >
-    <QuizQuestion
-      question={{
-        number: 1,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
-      }}
-      answers={[
-        {
-          id: "1",
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-        {
-          id: "2",
-          text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-        {
-          id: "3",
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-        },
-        {
-          id: "4",
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-      ]}
-    />
+    <Quiz />
   </ActivityLayout>
 );
 
