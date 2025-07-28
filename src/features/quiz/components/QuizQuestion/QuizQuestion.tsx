86b7facadaf19d26d25de77d8feff09eabc7ccc8
Alexandra <alexandrachrystal@gmail.com>
2025-07-28 16:32:07 +0700

    feat: add QuizQuestion component skeleton

---
+++ src/features/quiz/components/QuizQuestion/QuizQuestion.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+import { QuizQuestionWrapper } from "./style";
+import type { QuizQuestionProps } from "./types";
+
+export const QuizQuestion: FC<QuizQuestionProps> = () => {
+  return <QuizQuestionWrapper />;
+};
---
+++ src/features/quiz/components/QuizQuestion/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const QuizQuestionWrapper = styled.div``;
+export const AnswersList = styled.ul``;
+export const AnswerItem = styled.li``;
