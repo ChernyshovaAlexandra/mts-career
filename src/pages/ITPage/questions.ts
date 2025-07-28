83ca8dea74056533617a707bf58f493817305d4d
Alexandra Chernyshova <alexandrachrystal@gmail.com>
2025-07-28 22:04:03 +0700

    добавила тесты на все страницы активностей

--- src/pages/ITPage/questions.ts
+++
@@ -1,9 +0,0 @@
-import type { QuizQuestion } from "../../widgets/Quiz";
-
-export const itQuestions: QuizQuestion[] = [
-  {
-    question: "Что такое алгоритм?",
-    options: ["Последовательность действий", "Программа", "Устройство"],
-    correct: 0,
-  },
-];
