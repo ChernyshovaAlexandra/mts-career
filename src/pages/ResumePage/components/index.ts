349a52cf53f3d81abf2ce2bdaaf2c74395d71e06
BastianHolmes <sebastian.holmes2001@gmail.com>
2025-07-29 20:56:09 +0300

    feat: hr video block

--- src/pages/ResumePage/components/index.ts
+++ src/pages/ResumePage/components/index.ts
@@ -2,4 +2,6 @@ export { TipsGrid } from "./TipsGrid";
 export { ExpandableSection } from "./ExpandableSection";
 export { ResumeGame } from "./ResumeGame";
 export { MobileResumeGame } from "./MobileResumeGame";
-export { AIResumeChecker } from "./AIResumeChecker"; 
+export { AIResumeChecker } from "./AIResumeChecker";
+export { HRVideosCarousel } from "./HRVideosCarousel";
+export { VideoPlayer } from "./VideoPlayer"; 
